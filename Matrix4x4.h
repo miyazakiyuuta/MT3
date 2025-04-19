6ce519eac52bc3f7ceb4bb5252bca5ff383023c6
宮﨑遊太 <k024g0091@g.neec.ac.jp>
2025-04-20 02:06:51 +0900

    00_04の3次元での回転行列関数の追加

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -26,5 +26,11 @@ namespace MatrixMath {
 	Matrix4x4 MakeScaleMatrix(const Vector3& scale);
 	// 座標変換
 	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+	// X軸回転行列
+	Matrix4x4 MakeRotateXMatrix(float radian);
+	// Y軸回転行列
+	Matrix4x4 MakeRotateYMatrix(float radian);
+	// Z軸回転行列
+	Matrix4x4 MakeRotateZMatrix(float radian);
 
 }

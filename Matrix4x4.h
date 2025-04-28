085c348141c9fc5fb16527a61e1783d9f765bb21
宮﨑遊太 <k024g0091@g.neec.ac.jp>
2025-04-28 16:34:45 +0900

    01_01_三角形の描画とその移動と回転

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -42,5 +42,6 @@ namespace MatrixMath {
 	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
 	// ビューポート変換行列
 	Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+	
 
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <Novice.h>
 #include "Vector3.h"
 #include "Matrix4x4.h"
+#include <cstdint>
 
 const char kWindowTitle[] = "LE2B_24_ミヤザキ_ユウタ_タイトル";
 const int kWindowWidth = 1280; // 画面の横幅
 const int kWindowHeight = 720; // 画面の縦幅
+
+using namespace Vector3Math;
+using namespace MatrixMath;
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
@@ -18,8 +22,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	Vector3 rotate = {};
+	Vector3 translate = {};
+
+	//float radian = 0.0f;
+
+	Vector3 cameraPosition = {};
+
+	const Vector3 kLocalVertices[3] = {
+		{0.0f,0.1f,0.5f},
+		{0.1f,-0.1f,0.5f},
+		{-0.1f,-0.1f,0.5f}
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -34,6 +51,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 v1{ 1.2f,-3.9f,2.5f };
+		Vector3 v2{ 2.8f,0.4f,-1.3f };
+		Vector3 cross = Cross(v1, v2);
+
+		if (keys[DIK_W]) {
+			translate.z += 0.01f;
+		}
+		if (keys[DIK_S]) {
+
+			translate.z -= 0.01f;
+		}
+		if (keys[DIK_A]) {
+			translate.x -= 0.01f;
+		}
+		if (keys[DIK_D]) {
+			translate.x += 0.01f;
+		}
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+		// Y軸回転
+		rotate.y += 0.1f;
+
+		// 軸にしたい頂点を原点にする行列
+		Matrix4x4 moveToOriginMatrix = MakeTranslateMatrix({ -kLocalVertices[0].x,-kLocalVertices[0].y,-kLocalVertices[0].z });
+		// 回転行列
+		Matrix4x4 rotateMatrix = MakeRotateYMatrix(rotate.y);
+		// 原点にした処理を戻す行列
+		Matrix4x4 moveBackMatrix = MakeTranslateMatrix(kLocalVertices[0]);
+
+		//Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 worldMatrix = Multiply(Multiply(Multiply(moveToOriginMatrix, rotateMatrix), moveBackMatrix), translateMatrix);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f, }, { 0.0f,0.0f,0.0f }, cameraPosition);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+		Vector3 screenVertices[3];
+		for (uint32_t i = 0; i < 3; ++i) {
+			Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
+			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
+		}
 
 
 		///
@@ -44,7 +102,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		VectorScreenPrintf(0, 0, cross, "Cross");
 
+		Novice::DrawTriangle(
+			int(screenVertices[0].x), int(screenVertices[0].y),
+			int(screenVertices[1].x), int(screenVertices[1].y),
+			int(screenVertices[2].x), int(screenVertices[2].y),
+			RED, kFillModeSolid
+		);
 
 		///
 		/// ↑描画処理ここまで

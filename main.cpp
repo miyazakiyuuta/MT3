95c26784e73a4f0dd610181164cf8a2a6aa98ed4
宮﨑遊太 <k024g0091@g.neec.ac.jp>
2025-06-16 14:43:20 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Vector3.h"
 #include "Matrix4x4.h"
 #include <cmath>
+#include <imgui.h>
+#include <algorithm>
 
 const char kWindowTitle[] = "LE2B_24_ミヤザキ_ユウタ_タイトル";
 const int kWindowWidth = 1280; // 画面の横幅
@@ -27,24 +29,38 @@ struct Plane {
 	float distance; //!< 距離
 };
 
+struct Triangle {
+	Vector3 vertices[3]; //!< 頂点
+};
+
+struct AABB {
+	Vector3 min; //!< 最小点
+	Vector3 max; //!< 最大点
+	uint32_t color;
+};
+
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char type[]);
 
-void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
-void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, unsigned int color);
-
 Vector3 Project(const Vector3& v1, const Vector3& v2);
 Vector3 ClosestPoint(const Vector3& point, const Segment& segment);
+Vector3 Perpendicular(const Vector3& vector);
 
 bool IsCollision(const Sphere& s1, const Sphere& s2);
 bool IsCollision(const Sphere& sphere, const Plane& plane);
+bool IsCollision(const Segment& segment, const Plane& plane);
+bool IsCollision(const Triangle& triangle, const Segment& segment);
+bool IsCollision(const AABB& aabb1, const AABB& aabb2);
+bool IsCollision(const AABB& aabb, const Sphere& sphere);
+bool IsCollision(const AABB& aabb, const Segment& segment);
 
-Vector3 Perpendicular(const Vector3& vector);
-
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, unsigned int color);
 void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
-
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+void DrawAABB(const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -113,6 +129,157 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char type[]
 			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", matrix.m[row][column]);
 		}
 	}
+}
+
+Vector3 Project(const Vector3& v1, const Vector3& v2) {
+	Vector3 result =
+		Multiply(Dot(v1, v2) * (1.0f / (v2.x * v2.x + v2.y * v2.y + v2.z * v2.z)), v2);
+	return result;
+}
+
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
+	Vector3 result;
+	Vector3 project = Project(
+		{ point.x - segment.origin.x,point.y - segment.origin.y,point.z - segment.origin.z }, segment.diff);
+	result.x = segment.origin.x + project.x;
+	result.y = segment.origin.y + project.y;
+	result.z = segment.origin.z + project.z;
+	return result;
+}
+
+Vector3 Perpendicular(const Vector3& vector) {
+	if (vector.x != 0.0f || vector.y != 0.0f) {
+		return { -vector.y,vector.x,0.0f };
+	}
+	return { 0.0f,-vector.z,vector.y };
+}
+
+bool IsCollision(const Sphere& s1, const Sphere& s2) {
+	if (Length({ s1.center.x - s2.center.x,s1.center.y - s2.center.y ,s1.center.z - s2.center.z })
+		< s1.radius + s2.radius) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Sphere& sphere, const Plane& plane) {
+	// 球の中心位置と平面の法線ベクトルとの内積
+	float d = sphere.center.x * plane.normal.x +
+		sphere.center.y * plane.normal.y +
+		sphere.center.z * plane.normal.z;
+
+	// 球の中心から平面までの距離（符号付き）
+	float distance = fabs(d - plane.distance);
+
+	if (distance < sphere.radius) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Segment& segment, const Plane& plane) {
+	float dot = Dot(plane.normal, segment.diff);
+
+	// 垂直=平行であるので、衝突していない
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / (Dot(segment.diff, plane.normal));
+
+	if (t <= 1.0f && t >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Segment& segment) {
+	// 三角形の法線
+	Vector3 normal = Cross(
+		Subtract(triangle.vertices[1], triangle.vertices[0]),
+		Subtract(triangle.vertices[2], triangle.vertices[0]));
+	float dot = Dot(normal, segment.diff);
+	if (dot == 0.0f) {
+		return false;
+	}
+	float t = Dot(Subtract(triangle.vertices[0], segment.origin), normal) / dot;
+	if (t < 0.0f || t>1.0f) {
+		return false;
+	}
+
+	Vector3 p = Add(segment.origin, Multiply(t, segment.diff));
+
+	Vector3 cross01 = Cross(
+		Subtract(triangle.vertices[0], triangle.vertices[1]),
+		Subtract(triangle.vertices[1], p));
+	Vector3 cross12 = Cross(
+		Subtract(triangle.vertices[1], triangle.vertices[2]),
+		Subtract(triangle.vertices[2], p));
+	Vector3 cross20 = Cross(
+		Subtract(triangle.vertices[2], triangle.vertices[0]),
+		Subtract(triangle.vertices[0], p));
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const AABB& aabb1, const AABB& aabb2) {
+	if (aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x && // x軸
+		aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y && // y軸
+		aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z) { // z軸
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const AABB& aabb, const Sphere& sphere) {
+	// 最近接点を求める
+	Vector3 closestPoint{
+		std::clamp(sphere.center.x,aabb.min.x,aabb.max.x),
+		std::clamp(sphere.center.y,aabb.min.y,aabb.max.y),
+		std::clamp(sphere.center.z,aabb.min.z,aabb.max.z),
+	};
+	// 最近接点と球の中心との距離を求める
+	float distance = Length(Subtract(closestPoint, sphere.center));
+	// 距離が半径よりも小さければ衝突
+	if (distance <= sphere.radius) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const AABB& aabb, const Segment& segment) {
+
+	float txmin = (aabb.min.x - segment.origin.x) / segment.diff.x;
+	float txmax = (aabb.max.x - segment.origin.x) / segment.diff.x;
+
+	float tymin = (aabb.min.y - segment.origin.y) / segment.diff.y;
+	float tymax = (aabb.max.y - segment.origin.y) / segment.diff.y;
+
+	float tzmin = (aabb.min.z - segment.origin.z) / segment.diff.z;
+	float tzmax = (aabb.max.z - segment.origin.z) / segment.diff.z;
+
+	float tNearX = min(txmin, txmax);
+	float tFarX = max(txmin, txmax);
+	float tNearY = min(tymin, tymax);
+	float tFarY = max(tymin, tymax);
+	float tNearZ = min(tzmin, tzmax);
+	float tFarZ = max(tzmin, tzmax);
+
+	// AABBとの衝突点(貫通点)のtが小さい方
+	float tmin = max(max(tNearX, tNearY), tNearZ);
+	// AABBとの衝突点(貫通点)のtが大きい方
+	float tmax = min(min(tFarX, tFarY), tFarZ);
+	if (tmin <= tmax) {
+		if (tmin >= 0.0f && tmax <= 1.0f) {
+			return true;
+		}
+	}
+	return false;
 }
 
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
@@ -217,52 +384,6 @@ void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, con
 	}
 }
 
-Vector3 Project(const Vector3& v1, const Vector3& v2) {
-	Vector3 result =
-		Multiply(Dot(v1, v2) * (1.0f / (v2.x * v2.x + v2.y * v2.y + v2.z * v2.z)), v2);
-	return result;
-}
-
-Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
-	Vector3 result;
-	Vector3 project = Project(
-		{ point.x - segment.origin.x,point.y - segment.origin.y,point.z - segment.origin.z }, segment.diff);
-	result.x = segment.origin.x + project.x;
-	result.y = segment.origin.y + project.y;
-	result.z = segment.origin.z + project.z;
-	return result;
-}
-
-bool IsCollision(const Sphere& s1, const Sphere& s2) {
-	if (Length({ s1.center.x - s2.center.x,s1.center.y - s2.center.y ,s1.center.z - s2.center.z })
-		< s1.radius + s2.radius) {
-		return true;
-	}
-	return false;
-}
-
-bool IsCollision(const Sphere& sphere, const Plane& plane) {
-	// 球の中心位置と平面の法線ベクトルとの内積
-	float d = sphere.center.x * plane.normal.x +
-		sphere.center.y * plane.normal.y +
-		sphere.center.z * plane.normal.z;
-
-	// 球の中心から平面までの距離（符号付き）
-	float distance = fabs(d - plane.distance);
-
-	if (distance < sphere.radius) {
-		return true;
-	}
-	return false;
-}
-
-Vector3 Perpendicular(const Vector3& vector) {
-	if (vector.x != 0.0f || vector.y != 0.0f) {
-		return { -vector.y,vector.x,0.0f };
-	}
-	return { 0.0f,-vector.z,vector.y };
-}
-
 void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
 	Vector3 center = Multiply(plane.distance, plane.normal); // 1.中心点を決める
 	Vector3 perpendiculars[4];
@@ -282,3 +403,46 @@ void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const 
 	Novice::DrawLine(int(points[1].x), int(points[1].y), int(points[2].x), int(points[2].y), color);
 	Novice::DrawLine(int(points[1].x), int(points[1].y), int(points[3].x), int(points[3].y), color);
 }
+
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	Triangle screenTriangle;
+	screenTriangle.vertices[0] = Transform(Transform(triangle.vertices[0], viewProjectionMatrix), viewportMatrix);
+	screenTriangle.vertices[1] = Transform(Transform(triangle.vertices[1], viewProjectionMatrix), viewportMatrix);
+	screenTriangle.vertices[2] = Transform(Transform(triangle.vertices[2], viewProjectionMatrix), viewportMatrix);
+	for (int i = 0; i < 3; i++) {
+		Novice::DrawLine(
+			int(screenTriangle.vertices[i % 3].x), int(screenTriangle.vertices[i % 3].y),
+			int(screenTriangle.vertices[(i + 1) % 3].x), int(screenTriangle.vertices[(i + 1) % 3].y), color);
+	}
+}
+
+void DrawAABB(const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	Vector3 vertex[8];
+	vertex[0] = aabb.min;
+	vertex[1] = { aabb.max.x,aabb.min.y,aabb.min.z };
+	vertex[2] = { aabb.max.x,aabb.min.y,aabb.max.z };
+	vertex[3] = { aabb.min.x,aabb.min.y,aabb.max.z };
+	vertex[4] = { aabb.min.x,aabb.max.y,aabb.min.z };
+	vertex[5] = { aabb.max.x,aabb.max.y,aabb.min.z };
+	vertex[6] = aabb.max;
+	vertex[7] = { aabb.min.x,aabb.max.y,aabb.max.z };
+	Vector3 screenVertex[8];
+	for (int i = 0; i < 8; i++) {
+		screenVertex[i] = Transform(Transform(vertex[i], viewProjectionMatrix), viewportMatrix);
+	}
+	for (int i = 0; i < 4; i++) {
+		Novice::DrawLine(
+			int(screenVertex[i].x), int(screenVertex[i].y),
+			int(screenVertex[(i + 1) % 4].x), int(screenVertex[(i + 1) % 4].y),
+			color);
+		Novice::DrawLine(
+			int(screenVertex[i + 4].x), int(screenVertex[i + 4].y),
+			int(screenVertex[(i + 1) % 4 + 4].x), int(screenVertex[(i + 1) % 4 + 4].y),
+			color);
+		Novice::DrawLine(
+			int(screenVertex[i].x), int(screenVertex[i].y),
+			int(screenVertex[i + 4].x), int(screenVertex[i + 4].y),
+			color);
+	}
+}
+
